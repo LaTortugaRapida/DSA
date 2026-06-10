6af20d136796154f6ff62baf7c8b12cc5e5c48b3
LaTortugaRapida <nellitumasyan03@gmail.com>
2026-06-10 16:27:40 +0400

    Singly Linked List Implementation

---
+++ Data Structures/SinglyLinkedList.js
@@ -0,0 +1,217 @@
+class Node {
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
+class SinglyLinkedList {
+  #head;
+  constructor(head = null) {
+    this.#head = head;
+  }
+
+  empty() {
+    return this.#head === null;
+  }
+
+  size() {
+    if (this.empty()) return 0;
+    if (!this.#head.next) return 1;
+    let size = 0;
+    let node = this.#head;
+    while (node) {
+      ++size;
+      node = node.next;
+    }
+    return size;
+  }
+
+  clear() {
+    this.#head = null;
+  }
+
+  front() {
+    if (this.empty()) throw new Error("List is empty!");
+    return this.#head.value;
+  }
+
+  back() {
+    if (this.empty()) throw new Error("List is empty!");
+    let node = this.#head;
+    while (node.next) {
+      node = node.next;
+    }
+    return node.value;
+  }
+
+  at(index) {
+    if (!Number.isInteger(index) || index < 0)
+      throw new Error("Index must be a positive integer!");
+    if (this.empty()) throw new Error("List is empty!");
+    if (index >= this.size()) throw new Error("Invalid index!");
+    let node = this.#head;
+    while (index--) {
+      node = node.next;
+    }
+    return node.value;
+  }
+
+  pushFront(value) {
+    let newNode = new Node(value);
+    if (this.empty()) {
+      this.#head = newNode;
+      return;
+    }
+    let tmp = this.#head;
+    this.#head = newNode;
+    newNode.next = tmp;
+  }
+
+  pushBack(value) {
+    let newNode = new Node(value);
+    if (this.empty()) {
+      this.#head = newNode;
+      return;
+    }
+    let node = this.#head;
+    while (node.next) {
+      node = node.next;
+    }
+    node.next = newNode;
+  }
+
+  popFront() {
+    if (this.empty()) throw new Error("List is empty!");
+    if (this.size() === 1) {
+      let tmp = this.#head;
+      this.#head = null;
+      return tmp;
+    }
+    let tmp = this.#head;
+    this.#head = this.#head.next;
+    return tmp;
+  }
+
+  popBack() {
+    if (this.empty()) throw new Error("List is empty!");
+    if (this.size() === 1) {
+      let tmp = this.#head;
+      this.#head = null;
+      return tmp;
+    }
+    let node = this.#head;
+    while (node.next.next) {
+      node = node.next;
+    }
+    let tmp = node.next;
+    node.next = null;
+    return tmp;
+  }
+
+  insert(index, value) {
+    if (!Number.isInteger(index) || index < 0)
+      throw new Error("Index must be a positive integer!");
+    if (index > this.size()) throw new Error("Invalid index!");
+    if (index === 0) {
+      this.pushFront(value);
+      return;
+    }
+    let newNode = new Node(value);
+    let prev = this.#head;
+    while (index > 1) {
+      prev = prev.next;
+      --index;
+    }
+    let next = prev.next;
+    prev.next = newNode;
+    newNode.next = next;
+  }
+
+  erase(index) {
+    if (!Number.isInteger(index) || index < 0)
+      throw new Error("Index must be a positive integer!");
+    if (index >= this.size()) throw new Error("Invalid index!");
+    if (index === 0) {
+      return this.popFront();
+    }
+    if (index === this.size() - 1) {
+      return this.popBack();
+    }
+    let prev = this.#head;
+    while (index > 1) {
+      prev = prev.next;
+      --index;
+    }
+    let tmp = prev.next;
+    prev.next = prev.next.next || null;
+    return tmp;
+  }
+
+  find(value) {
+    if (this.empty()) throw new Error("List is empty!");
+    let node = this.#head;
+    let index = 0;
+    while (node) {
+      if (node.value === value) return index;
+      node = node.next;
+      ++index;
+    }
+
+    return -1;
+  }
+
+  contains(value) {
+    if (this.empty()) throw new Error("List is empty!");
+    let node = this.#head;
+    while (node) {
+      if (node.value === value) return true;
+      node = node.next;
+    }
+    return false;
+  }
+
+  toArray() {
+    if (this.empty()) return [];
+    let list = [];
+    let node = this.#head;
+    while (node) {
+      list.push(node.value);
+      node = node.next;
+    }
+    return list;
+  }
+
+  reverse() {
+    if (this.empty()) throw new Error("List is empty!");
+    if (this.size() === 1) return;
+    let cur = this.#head;
+    let next = cur.next;
+    let prev = null;
+    while (cur) {
+      next = cur.next;
+      cur.next = prev;
+      prev = cur;
+      cur = next;
+    }
+    this.#head = prev;
+  }
+
+  *[Symbol.iterator]() {
+    let node = this.#head;
+    while (node) {
+      yield node.value;
+      node = node.next;
+    }
+  }
+
+  *entries() {
+    let node = this.#head;
+    let index = 0;
+    while (node) {
+      yield [index++, node.value];
+      node = node.next;
+    }
+  }
+}
+
